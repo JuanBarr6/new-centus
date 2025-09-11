d60b8cc9b7efa0a7c4caf03590fecc109228440e
sebas.Opi.Tech <sebastian.alvarezo@opitech.com.co>
2025-09-11 11:44:25 -0500

    feat(sections): add menu, negotiation and alert sections

---
+++ features/sub-features/core/utils/constants/nav-section.ts
@@ -0,0 +1,21 @@
+export const menuSection = [
+  {
+    title: "Configuraciones",
+  },
+  {
+    title: "Modulos",
+  },
+];
+export const negociationSection = [
+  {
+    title: "Negociaciones",
+  },
+  {
+    title: "Integraciones",
+  },
+];
+export const alertSection = [
+  {
+    title: "Notificaciones",
+  },
+];
