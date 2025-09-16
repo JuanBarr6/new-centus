ebfd94449490e2a17f752ab0fb54f8b8e3ed00e6
sebas.Opi.Tech <sebastian.alvarezo@opitech.com.co>
2025-09-16 08:10:54 -0500

    feat(core): add LateralNavBar component with menu, negotiation and alerts sections

--- features/sub-features/core/components/organism/lateral-nav-bar.tsx
+++ features/sub-features/core/components/organism/lateral-nav-bar.tsx
@@ -1,1 +1,41 @@
+"use client";
+import NavSection from "../molecule/nav-sections";
+import {menuSection, negociationSection, alertSection} from "../../utils/constants/nav-section";
+import {Button} from "@/features/components/ui/button";
+import {Plus} from "lucide-react";
+import TitleSection from "../molecule/title-section";
 
+export default function LateralNavBar() {
+  return (
+    <>
+      <div className="h-full w-[331px] p-6">
+        <section className="scrollbar-none scrollbar flex h-[879px] w-[295px] flex-col items-center gap-2 overflow-y-auto rounded-[8px] border border-[var(--color-sidebar-ring)] bg-[var(--color-on-surface)] p-7 shadow-md">
+          <Button className="m-4 w-full cursor-pointer rounded-[42px] bg-[var(--color-secondary)] text-white uppercase hover:bg-[var(--secondary)]">
+            <Plus /> crear área
+          </Button>
+          <section className="flex w-full flex-col gap-4">
+            {menuSection.map(({title}) => (
+              <NavSection key={title} title={title} />
+            ))}
+          </section>
+          <hr className="my-2 border-t border-[var(--color-sidebar-ring)]" />
+
+          <section className="flex w-full flex-col gap-4">
+            <TitleSection title="Negociación" />
+            {negociationSection.map(({title}) => (
+              <NavSection key={title} title={title} />
+            ))}
+          </section>
+          <hr className="my-2 border-t border-[var(--color-sidebar-ring)]" />
+
+          <section className="flex w-full flex-col gap-4">
+            <TitleSection title="Alertas" />
+            {alertSection.map(({title}) => (
+              <NavSection key={title} title={title} />
+            ))}
+          </section>
+        </section>
+      </div>
+    </>
+  );
+}
