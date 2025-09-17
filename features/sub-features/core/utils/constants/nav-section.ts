6263686d6bae251d0a7e75c52cfc905757430ba1
sebas.Opi.Tech <sebastian.alvarezo@opitech.com.co>
2025-09-17 10:28:39 -0500

    feat(nav-sections): add icons to sections and introduce DropdownSection

--- features/sub-features/core/utils/constants/nav-section.ts
+++ features/sub-features/core/utils/constants/nav-section.ts
@@ -1,21 +1,55 @@
+import {
+  Wrench,
+  Boxes,
+  UserRoundCheck,
+  MessageSquareCode,
+  Bell,
+  GitFork,
+  Users,
+  UserRoundCog,
+} from "lucide-react";
+
 export const menuSection = [
   {
+    Icon: Wrench,
     title: "Configuraciones",
   },
   {
+    Icon: Boxes,
     title: "Modulos",
   },
 ];
 export const negociationSection = [
   {
+    Icon: UserRoundCheck,
     title: "Negociaciones",
   },
   {
+    Icon: MessageSquareCode,
     title: "Integraciones",
   },
 ];
 export const alertSection = [
   {
+    Icon: Bell,
     title: "Notificaciones",
+  },
+];
+
+export const DropdownSection = [
+  {
+    Icon: GitFork,
+    title: "Áreas",
+    value: "areas",
+  },
+  {
+    Icon: UserRoundCog,
+    title: "Roles",
+    value: "roles",
+  },
+  {
+    Icon: Users,
+    title: "Gestores",
+    value: "gestores",
   },
 ];
