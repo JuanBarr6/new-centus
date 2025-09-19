81a4b7af0372979f0c72ea41adaf3e1ed74e9919
alejandro <abel152_7@hotmail.com>
2025-09-19 14:14:25 -0500

    feat: avances otp

--- app/[local]/auth/otp/page.tsx
+++ app/[local]/auth/otp/page.tsx
@@ -1,3 +1,17 @@
-export default function OtpPage() {
-  return <h1>OTP Page</h1>;
+import SecurityCodeForm from "@/features/sub-features/auth/components/otp-form";
+ 
+export default function SecurityPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white px-4"> 
+     
+      <SecurityCodeForm />
+ 
+      <div className="mt-4 text-sm">
+        <span className="text-gray-500">¿Aún no recibes el código? </span>
+        <button className="text-purple-600 hover:underline">Reenviar ahora</button>
+      </div>
+    </div>
+  );
 }
+ 
+ 
