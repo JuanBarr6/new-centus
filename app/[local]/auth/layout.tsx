111c9fbc9b1ae4e6d40e751ed3efdf42ff7de261
alejandro <abel152_7@hotmail.com>
2025-09-29 12:45:47 -0500

    feat: refinamiento fondo otp

--- app/[local]/auth/layout.tsx
+++ app/[local]/auth/layout.tsx
@@ -2,10 +2,10 @@ interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
-export default function AuthLayout({children}: AuthLayoutProps) {
+export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="flex min-h-screen bg-white">
-      <div className="flex w-1/2 items-center justify-center">{children}</div>
+    <div className="w-full min-h-screen bg-white flex items-center justify-center">
+      {children}
     </div>
   );
 }
