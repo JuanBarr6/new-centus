701684e746608271dce379daae0c547c5bdfd44b
SebasAlvarez15 <sebastian.alvarezo@opitech.com.co>
2025-09-30 10:32:49 -0500

    feat(i18n): integrate auth and core message namespaces into locale files

--- messages/en.ts
+++ messages/en.ts
@@ -1,7 +1,9 @@
 import AuthMessagesEn from "../features/sub-features/auth/messages/en.json";
+import CoreMessagesEn from "../features/sub-features/core/messages/en.json";
 
 export default {
   ...AuthMessagesEn,
+  ...CoreMessagesEn,
   ExampleText: {
     label: "Hello world",
     greeting: "Hello, welcome to Centus!",
--- messages/es.ts
+++ messages/es.ts
@@ -1,7 +1,9 @@
 import AuthMessagesEs from "../features/sub-features/auth/messages/es.json";
+import CoreMessagesEs from "../features/sub-features/core/messages/es.json";
 
 export default {
   ...AuthMessagesEs,
+  ...CoreMessagesEs,
   ExampleText: {
     label: "Hola Mundo",
     greeting: "¡Hola, bienvenido a Centus!",
