dc6bd9c20172e2492a9d04415a99e8c90440ebce
SebasAlvarez15 <sebastian.alvarezo@opitech.com.co>
2025-09-30 10:26:32 -0500

    feat(i18n): add translation support for NavSection titles

--- features/sub-features/core/components/molecule/nav-sections.tsx
+++ features/sub-features/core/components/molecule/nav-sections.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import {Label} from "@radix-ui/react-label";
+import {getTranslations} from "next-intl/server";
 
 interface NavSectionProps {
   Icon: React.ElementType;
   title: string;
 }
 
-export default function NavSection({Icon, title}: NavSectionProps) {
+export default async function NavSection({Icon, title}: NavSectionProps) {
+  const t = await getTranslations("CoreMessages");
   return (
     <Link
       href="#"
       className="flex items-center gap-2 rounded-md border border-transparent px-4 py-2 hover:bg-gray-200"
     >
       <Icon className="text-primary h-5 w-5" />
-      <Label className="text-font-title text-sm font-medium">{title}</Label>
+      <Label className="text-font-title text-sm font-medium">{t(title)}</Label>
     </Link>
   );
 }
