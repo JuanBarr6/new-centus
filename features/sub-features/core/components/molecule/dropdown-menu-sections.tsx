b1c17ab13be0d8df4ec3ff552cf25c6c529d30e7
SebasAlvarez15 <sebastian.alvarezo@opitech.com.co>
2025-10-03 09:26:44 -0500

    style(dropdown-menu): update Accordion.Trigger with gradient background and improved hover state

--- features/sub-features/core/components/molecule/dropdown-menu-sections.tsx
+++ features/sub-features/core/components/molecule/dropdown-menu-sections.tsx
@@ -10,11 +10,26 @@ const DropdownMenuSections = () => {
   return (
     <Accordion.Root type="single" collapsible className="w-full">
       <Accordion.Item value="usuarios">
-        <Accordion.Trigger className="flex w-full items-center justify-between rounded-md bg-[var(--surface)] p-2 hover:bg-gray-200">
+        {/* <Accordion.Trigger
+          className={`data-[state=open]:border-primary flex w-full items-center justify-between rounded-md border border-transparent bg-[var(--surface)] p-2 hover:bg-gray-200 data-[state=open]:bg-gray-200`}
+        >
           <div className="text-font-title flex flex-row gap-3 font-medium">
-            <Users className="text-primary" /> {t("User")}
+            <Users className="text-primary" />
+            {t("User")}
           </div>
           <ChevronDown className="text-primary transition-transform data-[state=open]:rotate-180" />
+        </Accordion.Trigger> */}
+        <Accordion.Trigger
+          className={`relative flex w-full items-center justify-between rounded-md bg-[var(--surface)] p-[2px] hover:bg-gray-200 data-[state=open]:bg-gray-200 data-[state=open]:bg-gradient-to-r data-[state=open]:from-[#4919AB] data-[state=open]:to-[#CF34AA]`}
+        >
+          {/* Capa interna (fondo real) */}
+          <span className="flex w-full items-center justify-between rounded-md bg-[var(--surface)] p-2">
+            <div className="text-font-title flex flex-row gap-3 font-medium">
+              <Users className="text-primary" />
+              {t("User")}
+            </div>
+            <ChevronDown className="text-primary transition-transform data-[state=open]:rotate-180" />
+          </span>
         </Accordion.Trigger>
 
         <Accordion.Content className="mt-2 flex flex-col gap-2 px-3">
